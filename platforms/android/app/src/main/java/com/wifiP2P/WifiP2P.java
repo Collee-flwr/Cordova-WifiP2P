12e60ac63cd5197193d0b37e550a45afe1776fc1
Collee-flwr <campbell.colin.lee@gmail.com>
2021-01-21 21:05:37 -0600

    init discover

--- platforms/android/app/src/main/java/com/wifiP2P/WifiP2P.java
+++ platforms/android/app/src/main/java/com/wifiP2P/WifiP2P.java
@@ -1,12 +1,20 @@
 package com.wifiP2P;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.ActionListener;
+import android.net.wifi.p2p.WifiP2pManager.PeerListListener;
+import android.os.Looper;
+import android.util.Log;
 
 import org.apache.cordova.CordovaActivity;
 import org.apache.cordova.CordovaInterface;
@@ -17,6 +25,8 @@ import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.Collection;
 
 import static android.os.Looper.getMainLooper;
 
@@ -31,6 +41,7 @@ public class WifiP2P extends CordovaPlugin {
     WifiP2pManager.Channel channel;
     WiFiDirectBroadcastReceiver receiver;
     private final IntentFilter intentFilter = new IntentFilter();
+    Collection<WifiP2pDevice> devices;
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
@@ -38,7 +49,7 @@ public class WifiP2P extends CordovaPlugin {
         context = cordova.getContext();
         activity = (CordovaActivity) cordova.getActivity();
         manager = (WifiP2pManager) cordova.getActivity().getSystemService(Context.WIFI_P2P_SERVICE);
-        channel = manager.initialize(context, getMainLooper(), null);
+        channel = manager.initialize(context, Looper.getMainLooper(), null);
 
         // Indicates a change in the Wi-Fi P2P status.
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
@@ -55,16 +66,13 @@ public class WifiP2P extends CordovaPlugin {
     }
 
 
-
-
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("coolMethod")) {
             String message = args.getString(0);
             this.coolMethod(message, callbackContext);
             return true;
-        }
-        else if (action.equals("discover")){
+        } else if (action.equals("discover")) {
             this.discover(callbackContext);
             return true;
         }
@@ -79,10 +87,29 @@ public class WifiP2P extends CordovaPlugin {
         }
     }
 
-    private void discover(CallbackContext callbackContext){
+    @SuppressLint("MissingPermission")
+    private void discover(CallbackContext callbackContext) {
 
         receiver = new WiFiDirectBroadcastReceiver(callbackContext);
-        context.registerReceiver(receiver,intentFilter);
+        context.registerReceiver(receiver, intentFilter);
+        manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                // Code for when the discovery initiation is successful goes here.
+                // No services have actually been discovered yet, so this method
+                // can often be left blank. Code for peer discovery goes in the
+                // onReceive method, detailed below.
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+                // Code for when the discovery initiation fails goes here.
+                // Alert the user that something went wrong.
+            }
+        });
+
+
     }
 
     private class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
@@ -94,20 +121,29 @@ public class WifiP2P extends CordovaPlugin {
 
         }
 
+
+        @SuppressLint("MissingPermission")
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
 
-            if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
-                // Determine if Wifi P2P mode is enabled or not, alert
-                // the Activity.
-                int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
-                callbackContext.success(state);
+            if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+                    manager.requestPeers(channel, new PeerListListener() {
+                        @Override
+                        public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
+                            devices = wifiP2pDeviceList.getDeviceList();
+                            Log.d("onPeersAvailable: ", "DevicesList " + devices.size() );
+                            callbackContext.success(devices.size());
+                        }
+                    });
+                }
+
             }
 
         }
 
 
-    }
-
 }
+
+
